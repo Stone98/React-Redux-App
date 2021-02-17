64c353d4fb8e5dbd7cd2bbe86fcf5d7db7095147
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-02-17 14:01:08 -0700

    added image

--- app/src/App.js
+++ app/src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Ron Swanson Quotes!</h1>
+      <img src="https://www.gannett-cdn.com/-mm-/582568e00c17e48a137dbf529626145d89c4861c/c=0-151-3000-1846/local/-/media/2016/08/03/INGroup/Indianapolis/636058464185558674-parksandrec13.JPG?width=660&height=373&fit=crop&format=pjpg&auto=webp" alt="Ron Swanson" />
       <Quotes />
     </div>
   );
